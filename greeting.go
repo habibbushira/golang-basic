6041e52526ea923ef0c037a431adfd2d46b82083
Habba002 <habib.bushra002@gmail.com>
2023-07-27 06:45:04 +0300

    package scopes

---
+++ greeting.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+var points = []int{1, 2, 3, 4}
+
+func sayHello(n string) {
+	fmt.Println("hello ", n)
+}
+
+func showScore() {
+	fmt.Println("Score, ", score)
+}
---
+++ main_package.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+var score = 99.5
+
+func main() {
+	sayHello("maria")
+	showScore()
+
+	for _, v := range points {
+		fmt.Println(v)
+	}
+}
