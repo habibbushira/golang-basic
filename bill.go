7f682f58e1c7b19736d6750db4c8f4d42da28ace
Habba002 <habib.bushra002@gmail.com>
2023-07-29 06:36:19 +0300

    reciever function with pointer

--- bill.go
+++ bill.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type bill struct {
 	name  string
 	items map[string]float64
@@ -15,4 +17,32 @@ func newBill(name string) bill {
 	}
 
 	return b
+}
+
+// format the bill
+func (b *bill) format() string {
+	fs := "Bill breakdown: \n"
+
+	var total float64 = 0
+
+	// list item
+	for k, v := range b.items {
+		fs += fmt.Sprintf("%-25v ... %v\n", k+":", v)
+		total += v
+	}
+
+	// total
+	fs += fmt.Sprintf("%-25v ... $%0.2f\n", "tip:", b.tip)
+	fs += fmt.Sprintf("%-25v ... $%0.2f", "total:", total+b.tip)
+
+	return fs
+
+}
+
+func (b *bill) updateTip(t float64) {
+	b.tip = t
+}
+
+func (b *bill) addItem(name string, price float64) {
+	b.items[name] = price
 }
--- structs.go
+++ structs.go
@@ -8,6 +8,14 @@ func main() {
 
 	mybill := newBill("marios bill")
 
-	fmt.Println(mybill)
+	mybill.addItem("Oinion", 12)
+	mybill.addItem("Value", 2.3)
+	mybill.addItem("Banana", 1.2)
+	mybill.addItem("Orange", 3.3)
+	mybill.addItem("Mango", 6.7)
+
+	mybill.updateTip(10)
+
+	fmt.Println(mybill.format())
 
 }
